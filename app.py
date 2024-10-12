c683862a8028d135129671f7d9d770ff2ca96374
Shamsher Ghising Tamang <160412509+shamshertamang@users.noreply.github.com>
2024-10-12 01:30:13 -0400

    Initial commit for Waste Sort project

--- app.py
+++ app.py
@@ -1,18 +1,3 @@
-"""
-
-from flask import Flask
-
-app = Flask(__name__)
-
-
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
-
-
-if __name__ == '__main__':
-    app.run()
-"""
 from flask import Flask
 
 app = Flask(__name__)
@@ -23,4 +8,3 @@ def hello_world():
 
 if __name__ == '__main__':
     app.run(debug=True)
-
