c33762530f3e9b821b2074a9e1195c2b5e8d838f
Shamsher Ghising Tamang <160412509+shamshertamang@users.noreply.github.com>
2024-10-12 00:04:01 -0400

    Initial Flask app setup

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+"""
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -10,3 +12,15 @@ def hello_world():  # put application's code here
 
 if __name__ == '__main__':
     app.run()
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
